237a6662a324b29baf706d7b0fbd412915854391
Jamila McGowan <jamcarr280@gmail.com>
2023-04-08 00:15:27 -0700

    Ecercise 2 almost

--- app/models.py
+++ app/models.py
@@ -1,1 +1,35 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+from flask import url_for
+
+class Movie(db.Model):
+    
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique = True)
+    description = db.Column(db.Text)
+    poster=db.Column(db.String(200))
+    created_at=db.Column(db.DateTime(), nullable=False)
+    
+
+    def __init__(self, title, description, poster, created_at ):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        self.created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "poster": url_for('getImage', filename=self.poster),
+            "created_at": self.created_at
+        }  
+
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
